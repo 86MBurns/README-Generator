e535437ffd7b49cfb55fd0fce778cd18ba8c4d7f
Matt Burns <matt.burns01@gmail.com>
2022-08-20 08:17:11 -0700

    trying to get the answers to show up on the readme that is generated

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+const badges = ['[MIT License](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)','[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)','[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)','[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)','[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)','[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)','[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://img.shields.io/badge/License-GPL%20v2-blue.svg)','[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)','[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](http://www.gnu.org/licenses/lgpl-3.0)','[![License: FDL 1.3](https://img.shields.io/badge/License-FDL%20v1.3-blue.svg)](http://www.gnu.org/licenses/fdl-1.3)','[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)','[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)','[![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib)' ]
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if(!license === 'none'){
-    return '[MIT License](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)';
+    return (badges);
     
 }
-return '';
+return '[badges]';
 }
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -31,8 +32,13 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-`;
+  ${renderLicenseBadge(data.title)}
+    ##Discription
+    ${data.discripion}
+    ## Table Of Contents
+  ${renderLicenseLink(data.title)}
+  * [Insallation] (#install)
+   `;
 }
 
 module.exports = generateMarkdown;
